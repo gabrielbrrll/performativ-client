da0a353359d929d5f32988d99d9d9fa3105fabc8
gabrielbrrll <gabrielb.ocampo@gmail.com>
2025-01-19 17:18:54 +0800

    deploy ready vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
-// https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: '.vitest/setup',
-    include: ['**/test.{ts,tsx}']
+  server: {
+    host: true,
+    port: 3000
   }
 })
